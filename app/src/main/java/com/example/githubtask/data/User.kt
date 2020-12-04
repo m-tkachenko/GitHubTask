889884e8bd74fc4a6a704a74dbe49ad5a6e1e0f0
m-tkachenko <tk.mykhailo@gmail.com>
2020-12-04 13:29:41 +0100

    Data User Class

---
+++ app/src/main/java/com/example/githubtask/data/User.kt
@@ -0,0 +1,22 @@
+package com.example.githubtask.data
+
+data class User (
+    val current_user_url: String? = null,
+    val current_user_authorizations_html_url: String? = null,
+    val authorizations_url: String? = null,
+    val commit_search_url: String? = null,
+    val emails_url: String? = null,
+    val emojis_url: String? = null,
+    val events_url: String? = null,
+    val feeds_url: String? = null,
+    val issue_search_url: String? = null,
+    val issues_url: String? = null,
+    val label_search_url: String? = null,
+    val repository_url: String? = null,
+    val repository_search_url: String? = null,
+    val starred_url: String? = null,
+    val starred_gists_url: String? = null,
+    val user_url: String? = null,
+    val user_repositories_url: String? = null,
+    val user_search_url: String? = null
+    )
